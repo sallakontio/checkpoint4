52ef150cb4049cfea3d845b6a9968eb167416eb4
salla kontio <sallakatariinakontio@gmail.com>
2022-09-16 09:53:57 +0300

    Created 'create tableware' function, added some objects there

--- app.js
+++ app.js
@@ -3,9 +3,22 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const {} = require("./data");
+const { createTableware, readTableware } = require("./data");
 app.use(cors());
 app.use(express.json());
+
+// Read tableware
+app.get("/api/tableware", async (req, res) => {
+  console.log("Reading tableware at /api/tableware/");
+  return res.json(await readTableware());
+});
+
+// Create tableware
+app.post("/api/tableware", async (req, res) => {
+  console.log("Creating tableware at /api/tableware/");
+  await createTableware(req.body);
+  res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
--- data.js
+++ data.js
@@ -1,2 +1,19 @@
 const { Pool } = require("pg");
 const pool = new Pool();
+
+async function createTableware(tableware) {
+  await pool.query("insert into tableware (name, qty) values($1, $2)", [
+    tableware.name,
+    tableware.qty,
+  ]);
+}
+
+async function readTableware(tableware) {
+  const res = await pool.query("select * from tableware where id = $1", [id]);
+  return res.rows[0];
+}
+
+module.exports = {
+  createTableware,
+  readTableware,
+};
