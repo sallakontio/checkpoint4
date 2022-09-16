1b4a05e99c75cfb167233c891f4d765bc5852aad
salla kontio <sallakatariinakontio@gmail.com>
2022-09-16 10:31:16 +0300

    Task2: Added office_id to tableware, added foreign key and assigned all items to Espoo office

--- sqldataTask2.sql
+++ sqldataTask2.sql
@@ -11,3 +11,16 @@ insert into office (name, location, starting_year) values ('Academy Finland', 'E
 insert into office (name, location, starting_year) values ('Academy Sweden', 'Kista', 2015)
 insert into office (name, location, starting_year) values ('Academy Germany', 'Munchen', 2018)
 
+-- Add office_id column to tableware 
+alter table tableware add office_id int
+
+-- Add foreign key 
+alter table tableware add foreign key (office_id) references office(id)
+
+-- Updating tableware objects to belong to Espoo office
+update tableware set office_id = 1 where id = 1
+update tableware set office_id = 1 where id = 2
+update tableware set office_id = 1 where id = 3
+update tableware set office_id = 1 where id = 4
+update tableware set office_id = 1 where id = 5
+
